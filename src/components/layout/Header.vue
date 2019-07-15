48008ff4ad9c57501ede330aaa61e43ab2eb51e3
filipeaguiar <filipe.aguiar.cavalcanti@gmail.com>
2019-07-14 22:18:53 -0300

    Initial commit

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,26 @@
+<template>
+<header>
+  <nav>
+    <div class="nav-wrapper amber">
+      <a href="#" data-target="sidemenu" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <a href="#" class="brand-logo"><i class="material-icons">trending_up</i>Meta Breaker</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        
+      </ul>
+    </div>
+  </nav>
+  <ul class="sidenav" id="sidemenu"> 
+    <li><a href="">Modern</a></li>
+    <li><a href="">Pauper</a></li>
+    <li><a href="">Vintage</a></li>
+  </ul> 
+</header>
+</template>
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+<style>
+
+</style>
